6972e6f667a099afb90fd724e15b3c5fe93fb8a0
soh1b003 <sohibjinnorboboyev@gmail.com>
2024-10-18 19:37:05 +0500

    custom filter update

--- src/main/java/org/example/videoservice/filter/CustomFilter.java
+++ src/main/java/org/example/videoservice/filter/CustomFilter.java
@@ -13,11 +13,13 @@ import java.util.Objects;
 public class CustomFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String username = request.getHeader("Authorization");
+        String username = request.getHeader("X-Username");
         if (Objects.isNull(username)) {
             filterChain.doFilter(request, response);
             return;
         }
+
+
         UsernamePasswordAuthenticationToken auth =
                 new UsernamePasswordAuthenticationToken
                         (username, null, null);
