835598f4ec9bb034289a6f250fd8475ad0df84e7
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-11-06 21:41:41 +0500

    x

--- src/test/java/org/example/videoservice/VideoServiceApplicationTests.java
+++ src/test/java/org/example/videoservice/VideoServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.videoservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class VideoServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package org.example.videoservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class VideoServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
